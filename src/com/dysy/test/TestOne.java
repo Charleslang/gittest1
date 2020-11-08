b9e86ca878da2e69e10ba72a6055bfaad35b9d38
djf <1249954968@qq.com>
2020-11-08 21:31:24 +0800

    idea更新

--- src/com/dysy/test/TestOne.java
+++ src/com/dysy/test/TestOne.java
@@ -7,5 +7,6 @@ package com.dysy.test;
 public class TestOne {
     public static void main(String[] args) {
         System.out.println("hello word!");
+        System.out.println("idea update!");
     }
 }
